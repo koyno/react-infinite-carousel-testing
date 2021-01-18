6c9212c0df2520bcc2c6a0051b08f3a8ec95440e
Vlad Volkov <vlkv@tuta.io>
2021-01-18 22:38:38 +0300

    fix indendation

--- src/App.js
+++ src/App.js
@@ -4,44 +4,46 @@ import InfiniteCarousel from "react-leaf-carousel";
 export default function App() {
   return (
     <InfiniteCarousel
-    swipe
-    lazyLoad
-    dots
-    //showSides - library doesn't provide reliable way to differentiate side images from usual images 
-    breakpoints={[
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToScroll: 2,
-          slidesToShow: 2,
+      swipe
+      lazyLoad
+      dots
+      //showSides - library doesn't provide reliable way to differentiate side images from usual images
+      breakpoints={[
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToScroll: 2,
+            slidesToShow: 2,
+          },
         },
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToScroll: 3,
-          slidesToShow: 3,
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToScroll: 3,
+            slidesToShow: 3,
+          },
         },
-      },
-      {
-        breakpoint: 1600,
-        settings: {
-          slidesToScroll: 4,
-          slidesToShow: 4,
+        {
+          breakpoint: 1600,
+          settings: {
+            slidesToScroll: 4,
+            slidesToShow: 4,
+          },
         },
-      },
-    ]}
-    animationDuration={0}
-    slidesToScroll={6}
-    slidesToShow={6}
+      ]}
+      animationDuration={0}
+      slidesToScroll={6}
+      slidesToShow={6}
     >
-    
       {[...Array(24)].map((value, index) => (
         <div key={index}>
-          <img data-testid={"img" + index} src={"https://picsum.photos/200?random=" + index} alt="" />
+          <img
+            data-testid={"img" + index}
+            src={"https://picsum.photos/200?random=" + index}
+            alt=""
+          />
         </div>
       ))}
-
     </InfiniteCarousel>
   );
 }
